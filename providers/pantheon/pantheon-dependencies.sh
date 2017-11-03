7c6efae8dcf234526e621f9b3dd9545d2cce3ff4
Mike Schinkel <mike@newclarity.net>
2017-11-03 19:54:35 -0400

    Update Pantheon support to pull latest version of Terminus vs. fixed version.

--- providers/pantheon/pantheon-dependencies.sh
+++ providers/pantheon/pantheon-dependencies.sh
@@ -13,13 +13,16 @@ if ! [ -f ~/cache/terminus/bin/terminus ]; then
     # Installing Terminus for Pantheon
     #
     announce "...Installing Terminus for Pantheon"
+    mkdir -p ~/cache
     cd ~/cache
     rm -f terminus.tar.gz
     rm -rf terminus
-    wget -O terminus.tar.gz https://github.com/pantheon-systems/terminus/archive/1.6.0.tar.gz
+    TERMINUS_TARBALL="$(curl -s https://api.github.com/repos/pantheon-systems/terminus/releases/latest | jq -r ".tarball_url")"
+    wget -O terminus.tar.gz "${TERMINUS_TARBALL}"
     tar -xzf terminus.tar.gz
     rm terminus.tar.gz
-    mv terminus-1.6.0 terminus
+    TERMINUS_DIRECTORY="$(ls -d *terminus*/)"
+    mv "${TERMINUS_DIRECTORY}" terminus
     cd terminus
     composer update
 fi
@@ -36,3 +39,8 @@ sudo ln -s ~/cache/terminus/bin/terminus /usr/local/bin/terminus
 announce "...Authenticating Terminus for Pantheon"
 terminus auth:login --machine-token="${PANTHEON_TOKEN}"
 
+
+
+
+
+apt-get update && apt-get install -y curl jq wget
