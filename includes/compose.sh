6366298f1c67068734eaed4a48b2190b8ecc527e
Mike Schinkel <mike@newclarity.net>
2018-03-24 22:37:52 -0400

    Try deleting all files that are ignored by .gitignore just before running composer install.

--- includes/compose.sh
+++ includes/compose.sh
@@ -15,6 +15,20 @@ declare=${DEVOPS_ROOT:=}
 announce "Composing project"
 
 #
+# We need to get rid of files that are ignored by .gitignore
+#
+announce "...Get rid of files that are ignored by .gitignore"
+cd ${REPO_ROOT} >> $ARTIFACTS_FILE 2>&1
+rm .gitignore.bak >> $ARTIFACTS_FILE 2>&1
+mv .gitignore .gitignore.bak >> $ARTIFACTS_FILE 2>&1
+GITIGNORED_FILES="$(git status | grep "www/" | sed -e 's/^[[:space:]]*//')"
+for file in $GITIGNORED_FILES; do
+    announce "......Deleting ${file}"
+    rm -rf "${file}" >> $ARTIFACTS_FILE 2>&1
+done
+mv .gitignore.bak .gitignore >> $ARTIFACTS_FILE 2>&1
+
+#
 # Running Composer to install plugins and extra themes
 #
 announce "...Running Composer in ${REPO_ROOT} to install plugins and extra themes."
