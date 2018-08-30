29cef51b59e7b72d72a89e0b3afdeac1ec490125
Mike Schinkel <mike@newclarity.net>
2018-08-30 03:44:25 -0400

    Tweaking init.php again.

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 
 if ( ! is_file( $config_yaml = dirname( __DIR__ ) . '/config.yml' ) ) {
-	echo "Copying config.yml from WP DevOps to " . dirname( __DIR__ );
+	echo "Copying config.yml from WP DevOps into " . dirname( __DIR__ ) . '.';
 	copy( __DIR__ . '/config.yml', $config_yaml );
+	echo "\n";
 }
