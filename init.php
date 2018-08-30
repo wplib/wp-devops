5b8f193616d60238703aac38f6a56530ec8b3d58
Mike Schinkel <mike@newclarity.net>
2018-08-30 03:42:44 -0400

    Tweaking init.php

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 
 if ( ! is_file( $config_yaml = dirname( __DIR__ ) . '/config.yml' ) ) {
-	echo "Copying config.yml in project's .circleci directory";
+	echo "Copying config.yml from WP DevOps to " . dirname( __DIR__ );
 	copy( __DIR__ . '/config.yml', $config_yaml );
 }
