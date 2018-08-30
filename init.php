1bdaef1e66d19bc545a08aa541cd1be3b624ec8c
Mike Schinkel <mike@newclarity.net>
2018-08-30 04:21:40 -0400

    Adding output to init.php for the "do nothing" case.

--- init.php
+++ init.php
@@ -1,6 +1,8 @@
 <?php
 
-if ( ! is_file( $config_yaml = dirname( __DIR__ ) . '/config.yml' ) ) {
+if ( is_file( $config_yaml = dirname( __DIR__ ) . '/config.yml' ) ) {
+	echo "WP DevOps already initialized; doing nothing.";
+} else {
 	echo "Copying config.yml from WP DevOps into " . dirname( __DIR__ ) . '.';
 	copy( __DIR__ . '/config.yml', $config_yaml );
 	echo "\n";
